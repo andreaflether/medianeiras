dea052330c9cb60eafc01f0764d9a7bba4889649
Andréa Alencar <andreaflether@gmail.com>
2021-04-27 17:50:55 -0300

    Retornando para o AdminLTE3, reformulação nas views de usuário e organização nos assets (CSS/JS)

---
+++ src/app/assets/javascripts/config.js
@@ -0,0 +1,7 @@
+// Toastr config
+toastr.options = {
+  closeButton     : true,  // Adds a close button in the toast
+  extendedTimeOut : 3000,  // How long the toast will display after a user hovers over it
+  progressBar     : true,  // Visually indicate how long before a toast expires.
+  timeOut         : 6000,  // How long the toast will display without user interaction
+}
--- src/app/assets/javascripts/devise.js
+++ src/app/assets/javascripts/devise.js
@@ -1,23 +1,5 @@
-// require rails-ujs
-// require admin-lte/bower_components/jquery/dist/jquery.min
-// require admin-lte/bower_components/bootstrap/dist/js/bootstrap.min
-// require admin-lte/dist/js/adminlte.min
-// require admin-lte/plugins/iCheck/icheck.min
-// require toastr/build/toastr.min
-
-$(document).ready(function() {
-  $('input').iCheck({
-    checkboxClass : 'icheckbox_square-blue',
-    cursor        : true,
-    increaseArea  : '20%' /* optional */,
-    labelHover    : true,
-    radioClass    : 'iradio_square-blue',
-  });
-
-  toastr.options = {
-    extendedTimeOut   : 3000,
-    preventDuplicates : true,
-    progressBar       : true,
-    timeOut           : 7000,
-  }
-})
+//= require admin-lte/plugins/jquery/jquery.min
+//= require admin-lte/plugins/bootstrap/js/bootstrap.bundle.min
+//= require admin-lte/dist/js/adminlte.min
+//= require admin-lte/plugins/toastr/toastr.min
+//= require config
