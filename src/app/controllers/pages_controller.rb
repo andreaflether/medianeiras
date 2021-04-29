346f8d0f2088e375e6ce1e3bf00a244a7dd755c8
Andréa Alencar <andreaflether@gmail.com>
2021-04-29 19:34:33 -0300

    Add view para os itens restantes no navbar da página principal da aplicação

--- src/app/controllers/pages_controller.rb
+++ src/app/controllers/pages_controller.rb
@@ -2,10 +2,21 @@
 
 class PagesController < ApplicationController
   layout 'pages'
+
   def index
-    @main_activities = Activity.all.first(3)
-    @next_events = Event.all.happening_this_month
+    @main_activities = Activity.first(3)
+    @next_events = Event.happening_this_month
+                        .includes([:location])
+                        .first(5)
   end
+
+  def about; end
+
+  def activities; end
+
+  def partners; end
+
+  def events; end
 
   def contact; end
 end
