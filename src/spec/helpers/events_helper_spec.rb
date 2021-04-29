3d64664886b084af4a75e83a44bb3f0d753426e3
Andréa Alencar <andreaflether@gmail.com>
2021-04-29 19:54:52 -0300

    Add testes para helpers de Evento

---
+++ src/spec/helpers/events_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe EventsHelper, type: :helper do
+  context '#full_time' do 
+    let(:starts_at) { Time.now }
+    let(:ends_at) { Time.now + 1.hour }
+    let(:expected_time) { "#{starts_at.strftime('%H:%M') } às #{ends_at.strftime('%H:%M')}" }
+
+    it { expect(helper.full_time(starts_at, ends_at)).to eq(expected_time) }
+  end
+end
