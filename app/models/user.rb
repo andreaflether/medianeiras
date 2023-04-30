3232595e600957bce7246ec25a1be3b841e39ca8
Andréa Alencar <andreaflether@gmail.com>
2023-04-30 14:25:20 -0300

    Mudança no nome dos roles do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: %i[usuario admin superadmin]
+  enum role: %i[user admin superadmin]
 
   # Validations
   validates :full_name, presence: true, length: { minimum: 3, allow_blank: true }
 
   def admin?
-    !usuario?
+    !user?
   end
 end
